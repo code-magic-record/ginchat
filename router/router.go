b3de131ee12ee6801488e179dd5bb467cc223216
yaogengzhu <455947455@qq.com>
2023-10-09 13:56:09 +0800

    feat: 添加swagger

--- router/router.go
+++ router/router.go
@@ -1,13 +1,21 @@
 package router
 
 import (
+	"ginchat/docs"
 	"ginchat/service"
 
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.GET("/index", func(c *gin.Context) {
 		service.GetIndex(c)
 	})
