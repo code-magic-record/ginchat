b2f7485e142a6b89be493532650935736b3e4242
yaogengzhu <455947455@qq.com>
2023-10-15 21:55:06 +0800

    feat: 优化路由，补充中间件日志

--- router/index.go
+++ router/index.go
@@ -12,6 +12,10 @@ import (
 func Router() *gin.Engine {
 	r := gin.New()
 	docs.SwaggerInfo.BasePath = ""
+
+	// 使用重启和日志中间件
+	r.Use(gin.Recovery(), gin.Logger())
+
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// 将不同类型的路由分别放在不同的文件夹中
 	r.Any("/v1/user/*any", gin.WrapH(user.UserRouter()))
--- router/user/user.go
+++ router/user/user.go
@@ -10,14 +10,12 @@ import (
 func UserRouter() *gin.Engine {
 	r := gin.New()
 	v1 := r.Group("/v1/user")
-
 	v1.POST("/register", func(c *gin.Context) {
 		service.UserRegister(c)
 	})
 	v1.POST("/login", func(c *gin.Context) {
 		service.UserLogin(c)
 	})
-
 	v1.POST("/logout", middleware.AuthMiddleWare(), func(c *gin.Context) {
 		c.SetCookie("token", "", -1, "/", "localhost", false, true)
 		c.JSON(200, gin.H{
