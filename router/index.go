bdf4287eef8356efb1330a20471a486429f8b6a3
yaogengzhu <455947455@qq.com>
2023-10-25 22:08:41 +0800

    feat: 补充/路由信息

--- router/index.go
+++ router/index.go
@@ -16,6 +16,9 @@ func Router() *gin.Engine {
 
 	// 使用重启和日志中间件
 	r.Use(gin.Recovery(), gin.Logger())
+	r.GET("/", func(c *gin.Context) {
+		c.Writer.WriteString("<h1>hello go, 请访问 <a href='https://github.com/code-magic-record/ginchat'>https://github.com/code-magic-record/ginchat</a>查看源码</h1>")
+	})
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// 将不同类型的路由分别放在不同的文件夹中
